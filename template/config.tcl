20e1f2da24f4f88333a2ebc2910e5dabb677bbac
Tim <cpldcpu@gmail.com>
2022-08-31 21:36:25 +0200

    Add files via upload

--- template/config.tcl
+++ template/config.tcl
@@ -23,6 +23,8 @@ set ::env(DIE_AREA) "0 0 100 100"
 set ::env(FP_CORE_UTIL) 45
 set ::env(PL_BASIC_PLACEMENT) {1}
 set ::env(PL_TARGET_DENSITY) 0.8
+set ::env(BOTTOM_MARGIN_MULT) 2
+set ::env(TOP_MARGIN_MULT) 2
 
 # use alternative efabless decap cells to solve LI density issue
 set ::env(DECAP_CELL) "\
